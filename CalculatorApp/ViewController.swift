78d91384f25052b62deb221194189983fa05fc05
Kaei <eifimeimeidola@gmail.com>
2018-11-14 21:17:07 +0900

    UI

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func inputFormula(_ sender: Any) {
+    }
+    
 }
 
